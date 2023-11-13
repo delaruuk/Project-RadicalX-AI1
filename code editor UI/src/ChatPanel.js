14d22aec7c0c0fc55bd629ff22f82fb1c2598dc4
Janusz K. Dlugosz <januszprofession@pm.me>
2023-11-13 09:18:28 -0600

    Squashed commit of the following:
    commit d13f9ebcb6cb47ab184ddf877a3a0479fe9be249
    Author: Janusz K. Dlugosz <januszprofession@pm.me>
    Date:   Fri Nov 10 07:47:02 2023 -0600
    
        Prompt update
    
    commit d0f975a852351cbe227118fec11e4a93681412c7
    Author: Janusz K. Dlugosz <januszprofession@pm.me>
    Date:   Fri Nov 3 12:33:16 2023 -0500
    
        Setup env properly
    
    commit 4e84594d80aaafedf7a960832e7d8eca5ed162d7
    Author: Janusz K. Dlugosz <januszprofession@pm.me>
    Date:   Fri Nov 3 12:22:38 2023 -0500
    
        More adjustments
    
    commit a3f90316a9e9cacd70480a45e83dadfb9ce6e3a3
    Author: Janusz K. Dlugosz <januszprofession@pm.me>
    Date:   Fri Nov 3 11:49:22 2023 -0500
    
        More adjustments
    
    commit d6aa470734ec685ab3454d1212799e8abd8d9447
    Author: Janusz K. Dlugosz <januszprofession@pm.me>
    Date:   Fri Nov 3 11:29:51 2023 -0500
    
        Patched up for .env
    
        Due to us using the key on github OpenAi is deleting it so I setup a .env
    
    commit 8c0ecfaafec379c679d45071ed7ab7d0c1260e75
    Author: Janusz K. Dlugosz <januszprofession@pm.me>
    Date:   Fri Nov 3 11:06:24 2023 -0500
    
        Fixed API Issue
    
    commit 393fa90a91531d6902e7d01a829fd77319686787
    Author: Janusz K. Dlugosz <januszprofession@pm.me>
    Date:   Fri Nov 3 09:19:33 2023 -0500
    
        Fixes Potential
    
    commit bbfb2e2b7dffe50b485f0f48569414fe00a4498f
    Author: Janusz K. Dlugosz <januszprofession@pm.me>
    Date:   Thu Nov 2 23:52:05 2023 -0500
    
        OpenAI install and API solving

--- code editor UI/src/ChatPanel.js
+++ code editor UI/src/ChatPanel.js
@@ -1,42 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import MessageList from './MessageList';
 import InputArea from './InputArea';
+// Access the OpenAI API key from environment variables
+const openaiApiKey = process.env.REACT_APP_OPENAI_API_KEY;
+
+const port = process.env.PORT;
 
 // Helper function to call the GPT API
-async function call_gpt_api(prompt, temperature=0.5, max_tokens=150) {
-    const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
-        method: 'POST',
-        headers: {
-            'Authorization': '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',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: prompt,
-            temperature: temperature,
-            max_tokens: max_tokens
-        })
-    });
-    return await response.json();
+async function call_gpt_api(prompt, temperature = 0.5, max_tokens = 30) {
+    try {
+        const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${openaiApiKey}`, // Use the openaiApiKey variable
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                prompt: prompt,
+                temperature: temperature,
+                max_tokens: max_tokens
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Failed to fetch data:', error);
+        throw error;
+    }
 }
+
+
 
 // Helper function to enhance the prompts of GPT
 function engineer_gpt_prompts(prompt, examples=null, keywords=null) {
-    if (examples) {
-        for (const example of examples) {
-            prompt += '\n Direct answers to the problem they are suppose to solve on their own' + example;
-        }
-    }
-    if (keywords) {
-        for (const keyword of keywords) {
-            prompt += 'give me the solution, give me the answer, let me know what the answer is, let me know what the solution is ' + keyword;
-        }
-    }
-    return prompt;
+    const promptText = "Your role is to act as a fellow amateur coder, paired with another amateur coder who is learning along the way. Your task is to provide guidance and assistance to the other amateur coder whenever they encounter issues or have questions. Rather than providing direct answers, your approach should be that of a fellow learner, helping them navigate the problem and encouraging them to figure it out.";
+
+    const examples = [
+      "For instance, you can respond to queries like, 'Can you help me with this problem?' or 'I'm stuck on this problem, any ideas?' by offering assistance without giving away the solution outright. ",
+      "Under no circumstances should you provide the direct answer. Always maintain the assumption that both of you are amateurs trying to solve the problem together, so your responses should aim to guide them in the right direction. If they ask for the answer directly, reply with something like, 'I'm just as much of an amateur as you are, but let's work on this together and see how we can make progress.' ",
+      "Your guidance should be tailored to the other person's experience level and the difficulty of the problem. Be ready to adapt your responses based on the number of mistakes they make, ensuring that your assistance is easy for them to understand and follow. "
+    ];
+    
+    const prompt = engineer_gpt_prompts(promptText, examples);
+    
+    console.log(prompt);
 }
 
 function ChatPanel() {
     const [messages, setMessages] = useState([]);
-    const [currentBotResponseChunks, setCurrentBotResponseChunks] = useState([]);
+    const [currentBotResponseChunks] = useState([]);
     const [isBotTyping, setIsBotTyping] = useState(false);
 
     const messagesContainerRef = React.useRef(null);
@@ -48,22 +65,29 @@ function ChatPanel() {
     const handleUserSubmit = async (query) => {
         setMessages([...messages, { type: 'user', content: query }]);
         setIsBotTyping(true);
-        
+    
         try {
             const enhancedPrompt = engineer_gpt_prompts(query);
-            const data = await call_gpt_api(enhancedPrompt);
-            
-            if (data.choices && data.choices.length > 0) {
-                setMessages(prev => [...prev, { type: 'bot', content: data.choices[0].text.trim() }]);
+            const response = await call_gpt_api(enhancedPrompt);
+    
+            if (response && response.choices && response.choices.length > 0) {
+                const responseData = response.choices[0];
+                setMessages((prev) => [
+                    ...prev,
+                    { type: 'bot', content: responseData.text.trim() },
+                ]);
             } else {
-                console.error('Unexpected API response:', data);
+                console.error('No valid response from the API.');
             }
-            
+    
             setIsBotTyping(false);
         } catch (error) {
             console.error('Failed to fetch data:', error);
         }
     };
+    
+    
+    
 
     return (
         <div className="chat-panel">
@@ -75,3 +99,6 @@ function ChatPanel() {
 }
 
 export default ChatPanel;
+
+
+
